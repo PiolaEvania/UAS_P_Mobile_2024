5e8478c5521770d33db067eff81f88ce055b9f5d
PiolaEvania <piolaevania@gmail.com>
2024-07-01 07:48:54 +0800

    mengupdate forget password

--- lib/ui/screen/user/forgot_password_screen.dart
+++ lib/ui/screen/user/forgot_password_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../data/auth/auth_service.dart';
-import 'login_screen.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   @override
